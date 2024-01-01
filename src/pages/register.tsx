969aa419f9cf1bfeabe9119cbcc3f63f5d3a82ee
npark11 <nari36@naver.com>
2024-01-01 18:37:51 -0500

    first commit

---
+++ src/pages/register.tsx
@@ -0,0 +1,51 @@
+import InputGroup from "@/components/InputGroup";
+import Link from "next/link";
+import { useState } from "react";
+
+const Register = () => {
+  const [email, setEmail] = useState("");
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const [errors, setErrors] = useState<any>({});
+
+  return (
+    <div className="bg-white">
+      <div className="flex flex-col items-center justify-center h-screen p-6">
+        <div className="w-10/12 mx-auto md:w-96">
+          <h1 className="mb-2 text-lg font-medium">Register</h1>
+          <form>
+            <InputGroup
+              placeholder="Email"
+              value={email}
+              setValue={setEmail}
+              error={errors.email}
+            />
+            <InputGroup
+              placeholder="Username"
+              value={username}
+              setValue={setUsername}
+              error={errors.username}
+            />
+            <InputGroup
+              placeholder="Password"
+              value={password}
+              setValue={setPassword}
+              error={errors.password}
+            />
+            <button className="w-full py-2 mb-1 text-xs font-bold text-white uppercase bg-gray-400 border border-gray-400 rounded">
+              Create your account
+            </button>
+          </form>
+          <small>
+            Already a member?
+            <Link href="/login" className="ml-1 text-blue-500">
+              Login
+            </Link>
+          </small>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Register;
